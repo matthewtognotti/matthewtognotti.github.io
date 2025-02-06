62f320e2103ecc31d84b8d9ed734a4eec76699b7
Matt Tognotti <matthewtognotti@gmail.com>
2025-02-06 09:25:15 -0800

    Update scroll offset for smooth scrolling and IDs

--- scroll.js
+++ scroll.js
@@ -4,7 +4,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       const target = document.querySelector(this.getAttribute("href"));
       if (target) {
         window.scrollTo({
-          top: target.offsetTop - 50, // Adjust offset as needed
+          top: target.offsetTop - 75, // Adjust offset as needed
           behavior: "smooth"
         });
       }
